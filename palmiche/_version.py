4d7e84cccefd54c2164199354950b3b6cc64488f
ale94mleon <ale94mleon@gmail.com>
2022-10-13 12:00:50 +0200

    fixing versioning

--- palmiche/_version.py
+++ palmiche/_version.py
@@ -1,5 +1,13 @@
 # coding: utf-8
 
 # We will use semantic version (major, minor, patch)
-__version_tuple__ = version_tuple = (0, 0, 0,'-',0,0,1)
-__version__ = version = '.'.join([str(i) for i in __version_tuple__])
+__main_version_tuple__ = main_version_tuple = (0, 0, 0)
+__pre_version_tuple__ = pre_version_tuple = ('',0,0,1) # or None or an empty tuple, the first char is alpha, '', beta, rc, etc...
+if __pre_version_tuple__:
+    __version_tuple__ = version_tuple = tuple(list(__main_version_tuple__) + list(__pre_version_tuple__))
+    __version__ = version = '.'.join([str(i) for i in __main_version_tuple__]) + f'-{__pre_version_tuple__[0]}' + '.'.join([str(i) for i in __pre_version_tuple__[1:]])
+else:
+     __version__ = version = '.'.join([str(i) for i in __main_version_tuple__])
+
+
+print(version)
